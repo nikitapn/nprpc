d8048fa9d64fdb4859ad9152fe851b26c690aef9
Nikita Pennie <nikitapnn1@gmail.com>
2026-03-01 13:30:55 +0100

    fix

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-cmake --build .build_release --target=nprpc_benchmarks -j$(nproc) # > /dev/null
-killall benchmark_server npnameserver grpc_benchmark_server capnp_benchmark_server 2>/dev/null || true
-./.build_release/benchmark/nprpc_benchmarks $@
+cmake --build .build_release --target=nprpc_benchmarks -j$(nproc) 2>&1 > /dev/null
+killall benchmark_server npnameserver grpc_benchmark_server capnp_benchmark_aserver 2>/dev/null || true
+./.build_release/benchmark/nprpc_benchmarks $@ 2>&1 | awk '/----------/{f=1} /Shutdown requested/{f=0} f'
 
 # Example usage:
 # ./run_benchmark.sh --benchmark_filter="LatencyFixture/LargeData1MB/0"
